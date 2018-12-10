cc2d3da639e465e722110616af1b2e1efc3d54f3
Yuming Su <877385836@qq.com>
2018-12-10 22:03:44 +0000

    Debugging 1.0

---
+++ app.js
@@ -0,0 +1,82 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const imitations = require("./routes/imitations");
+const factories = require("./routes/factories");
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.get('/imitations', imitations.findAll);
+app.get('/imitations/reports', imitations.findTotalReports);
+app.get('/imitations/:id', imitations.findOne);
+app.get('/imitations/brand', imitations.findByBrand);
+app.get('/factories', factories.findAll);
+app.get('/factories/:id', factories.findOne);
+app.get('/factories/reports', factories.findTotalReports);
+
+app.post('/imitations',imitations.addImitation);
+app.post('/factories',factories.addFactory);
+
+app.put('/imitations/:id/report', imitations.incrementReports);
+app.put('/factories/:id/report', factories.incrementReports);
+
+
+app.delete('/factories/:id', factories.deleteFactory);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'dev') {
+    app.use(function(err, req, res) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+if (process.env.NODE_ENV !== 'test') {
+    app.use(logger('dev'));
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
+
---
+++ routes/imitations.js
@@ -0,0 +1,127 @@
+let Imitation = require('../models/imitations');
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+var mongodbUri ='mongodb://imitationsdb:LLEAFE8q4@ds139883.mlab.com:39883/imitationsdb';
+
+mongoose.connect(mongodbUri);
+let db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('Unable to Connect to [ ' + db.name + ' ]', err);
+});
+
+db.once('open', function () {
+    console.log('Successfully Connected to [ ' + db.name + ' ]');
+});
+
+router.findAll = (req, res) => {
+    // Return a JSON representation of our list
+    res.setHeader('Content-Type', 'application/json');
+
+    Imitation.find(function(err, imitations) {
+        if (err)
+            res.send(err);
+
+        res.send(JSON.stringify(imitations,null,5));
+    });
+}
+
+router.findOne = (req, res) => {
+
+    res.setHeader('Content-Type', 'application/json');
+
+    Imitation.find({ "_id" : req.params.id },function(err, imitation) {
+        if (err)
+            res.send('Imitations NOT FOUND!!');
+        else
+            res.send(err);
+    });
+}
+router.findByBrand = (req,res) =>{
+    res.setHeader('Content-Type', 'application/json');
+
+    var imitation = getByBrand(imitations,req.params.brand);
+
+    if (imitation != null)
+        res.send(JSON.stringify(imitation,null,5));
+    else
+        res.send('Imitations NOT Found!!');
+
+
+}
+
+function getByValue(array, id) {
+    var result  = array.filter(function(obj){return obj.id == id;} );
+    return result ? result[0] : null; // or undefined
+}
+
+
+function getTotalReports(array) {
+    let totalReports = 0;
+    array.forEach(function(obj) { totalReports += obj.reports; });
+    return totalReports;
+}
+
+router.addImitation = (req, res) => {
+
+    res.setHeader('Content-Type', 'application/json');
+
+    var imitation = new Imitation();
+
+    imitation.brand = req.body.brand;
+    imitation.type = req.body.type;
+    imitation.price = req.body.price;
+
+    imitation.save(function(err) {
+        if (err)
+            res.json({ message: 'Imitation NOT Added!', errmsg : err } );
+        else
+            res.json({ message: 'Imitation Successfully Added!', data: imitation });
+    });
+}
+
+router.incrementReports = (req, res) => {
+
+    Imitation.findById(req.params.id, function(err,imitation) {
+        if (err) {
+            res.status(404);
+            res.json({message: 'Imitation NOT Reported Successfully!!'});
+        }
+        else {
+            imitation.reports += 1;
+            imitation.save(function (err) {
+                if (err){
+                    res.status(404)
+                    res.json({message:'Invalid Imitation!! NOT Reported Successfully!!'});
+                }
+                else
+                    res.json({message:'Imitation Reported Successfully!!'});
+            });
+        }
+    });
+}
+
+router.deleteImitation = (req, res) => {
+
+    Imitation.findByIdAndRemove(req.params.id, function(err) {
+        if (err)
+            res.json({ message: 'Imitation NOT DELETED!', errmsg : err } );
+        else
+            res.json({ message: 'Imitation Successfully Deleted!'});
+    });
+}
+
+router.findTotalReports = (req, res) => {
+
+    Imitation.find(function(err, imitations) {
+        if (err)
+            res.send(err);
+        else
+            res.json({ totalReports : getTotalReports(imitations) });
+    });
+}
+
+
+module.exports = router;
